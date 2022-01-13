d937608569d169ba911d658729b1ce7e7d3ad607
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2022-01-12 23:32:13 -0500

    Vuex Fundamentos
    Se trabajó con los principales componentes de VUEX entendiendo sus fundamentos y funciones.

--- Proyecto_CLI/practica_vuex-1/src/store/index.js
+++ Proyecto_CLI/practica_vuex-1/src/store/index.js
@@ -2,11 +2,30 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    contador: 260
+    contador: 80
   },
   mutations: {
+    incrementar(state, payload) {
+      state.contador = state.contador + payload
+    },
+    disminuir(state, payload) {
+      state.contador = state.contador - payload
+    }
   },
   actions: {
+    accionIncrementar({commit}) {
+      commit('incrementar', 10)
+    },
+    accionDisminuir({commit}, numero) {
+      commit('disminuir', numero)
+    },
+    accionBoton({commit}, objeto) {
+      if(objeto.estado) {
+        commit('incrementar', objeto.numero)
+      }else {
+        commit('disminuir', objeto.numero)
+      }
+    }
   },
   modules: {
   }
--- Proyecto_CLI/practica_vuex-1/src/views/Home.vue
+++ Proyecto_CLI/practica_vuex-1/src/views/Home.vue
@@ -5,14 +5,22 @@
       :style="colorContador"
     >
     {{titulo}}: {{ contador }}</h1>
+    <button @click="accionIncrementar">Aumentar</button>
+    <BtnDisminuir />
+    <hr>
+    <BotonAccion :estado="true"/>
+    <BotonAccion :estado="false"/>
   </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
+import BtnDisminuir from '../components/BtnDisminuir'
+import BotonAccion from '../components/BotonAccion'
+import {mapMutations, mapState, mapActions} from 'vuex'
 export default {
   name: 'Home',
   components: {
+    BtnDisminuir, BotonAccion
     
   },
   data() {
@@ -21,10 +29,15 @@ export default {
     }
   },
   computed: {
-    ...mapState(['contador']),
+    ...mapState(['contador', mapMutations]),
     colorContador(){
       return [this.contador > 100 ? {'color':'green'} : {'color':'red'}]
     }
+  },
+  methods: {
+    ...mapMutations(['incrementar']),
+    ...mapActions(['accionIncrementar'])
+    
   }
 }
 </script>
