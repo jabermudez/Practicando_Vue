cdd88f0fd155bf31bcea1cf352dbccebc7672903
Jose Alexander Bermudez <jabermudez99@misena.edu.co>
2022-01-15 22:25:39 -0500

    COMPOSITION API
    Se trabajaron los fundamentos de Composition AP en Vue. Donde se consumió un API de Países con sus detalles.

---
+++ proyecto_composition_api/composition_1/src/components/Titulo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1
+    :style="{'color': color}"
+  >Contador: {{ signoPesos }}
+  </h1>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+export default {
+    props: ['contador', 'color'],
+    setup(props) {
+        const signoPesos = computed(() => {
+            return '$' + props.contador
+        })
+        return {signoPesos}
+    }
+}
+</script>
+
---
+++ proyecto_composition_api/composition_1/src/views/Paises.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Lista de Paises</h1>
+  <p v-for="(pais, index) in arrayData" :key="index">      
+      <router-link :to="`/paises/${pais.name}`">
+          {{ pais.name }}
+      </router-link>
+  </p>
+</template>
+
+<script>
+import {userFetch} from '../hooks/userFetch'
+export default {
+    setup() {
+        return {...userFetch('api.json')}
+    }
+}
+</script>
+
